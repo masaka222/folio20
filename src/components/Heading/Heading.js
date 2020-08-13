1a4ab6ff7644b13f51233e55938dbec013249879
Vasilis Georgoudis <masakaftw@hotmail.com>
2020-08-13 20:55:16 +0300

    first page initial design

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -4,7 +4,18 @@ import './Heading.css';
 const Heading = () => {
     return(
         <div className='Heading'>
-            heading
+            <div className='heading_main'>
+                <div className='item-small back'>Back to all projects</div>
+                <div className='archive'>Archive</div>
+            </div>
+            <div className='heading_footer'>
+                <div className='arrow'>&#8595;</div>
+                <div className='alpha'>A.</div>
+                <div className='alpha_text'> 
+                    <div style={{fontWeight: 'bold'}}>Work places archive</div>
+                    <div className='item-small'>Something different</div>
+                </div>
+            </div>
         </div>
     );
 }
